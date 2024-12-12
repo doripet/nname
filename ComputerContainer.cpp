03485888305f45b753260ae33d911d03c1ecb364
Айтанова Дарина <darinaajtanova@gmail.com>
2024-12-12 21:01:48 +0400

    проверка ignЭ

--- ComputerContainer.cpp
+++ ComputerContainer.cpp
@@ -18,6 +18,7 @@ void ComputerContainer::removeComputer(int index) {
     }
 }
 //проверка открытия файла
+//checkk111
 void ComputerContainer::loadFromFile(const std::string& filename) {
     std::ifstream file(filename);
     if (!file.is_open()) {
