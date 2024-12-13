95e3e0535184196039c2ebbafb0a7b84e9a339d4
Айтанова Дарина <darinaajtanova@gmail.com>
2024-12-13 10:41:21 +0400

    hhh

--- ComputerContainer.cpp
+++ ComputerContainer.cpp
@@ -18,7 +18,7 @@ void ComputerContainer::removeComputer(int index) {
     }
 }
 //проверка открытия файла
-//CHECK23444s 
+//check
 void ComputerContainer::loadFromFile(const std::string& filename) {
     std::ifstream file(filename);
     if (!file.is_open()) {
